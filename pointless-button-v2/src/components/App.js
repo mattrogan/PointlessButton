866e82282afc55e9abbe8c55c3bb25904211e61e
mattrogan <Matt.Rogan2@Gmail.com>
2022-12-30 21:21:50 +0000

    Transfer files from legacy version

---
+++ pointless-button-v2/src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Footer from './footer/Footer';
+
+const useState = React.useState
+
+function App() {
+    function increment() {
+        setNumClicks(function (prev) {
+            return prev + 1
+        })
+    }
+
+    const [numClicks, setNumClicks] = useState(0);
+
+    return (
+        <div className="gameplay">
+            <p className="counter">You have clicked {numClicks} times.</p>
+            <p>Click at your own risk...</p>
+            <br />
+            <button className="button" type="button" onclick={increment}>Click me?</button>
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
